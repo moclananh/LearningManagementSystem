c8691ab88317bfbd3925c09e8d1d31491ce0ccc4
ThanhTHCE160136 <kristino5223@gmail.com>
2023-02-24 09:02:47 +0000

    Group 02 - Update paging quizz

--- Applications/Repositories/IQuizzRepository.cs
+++ Applications/Repositories/IQuizzRepository.cs
@@ -6,7 +6,7 @@ namespace Applications.IRepositories
 {
     public interface IQuizzRepository : IGenericRepository<Quizz>
     {
-        Task<Pagination<Quizz>> GetQuizzByUnitIdAsync(Guid UnitId);
+        Task<Pagination<Quizz>> GetQuizzByUnitIdAsync(Guid UnitId, int pageNumber = 0, int pageSize = 10);
         Task<Pagination<Quizz>> GetQuizzByName(string Name, int pageNumber = 0, int pageSize = 10);
         Task<Pagination<Quizz>> GetEnableQuizzes(int pageNumber = 0, int pageSize = 10);
         Task<Pagination<Quizz>> GetDisableQuizzes(int pageNumber = 0, int pageSize = 10);
--- Infrastructures/Repositories/QuizzRepository.cs
+++ Infrastructures/Repositories/QuizzRepository.cs
@@ -3,6 +3,7 @@ using Applications.Interfaces;
 using Applications.IRepositories;
 using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
+using System.Drawing.Printing;
 
 namespace Infrastructures.Repositories
 {
@@ -78,14 +79,25 @@ namespace Infrastructures.Repositories
             return result;
         }
 
-        public Task<Pagination<Quizz>> GetQuizzByUnitIdAsync(Guid UnitId)
+        public async Task<Pagination<Quizz>> GetQuizzByUnitIdAsync(Guid UnitId, int pageNumber = 0, int pageSize = 10)
         {
-            throw new NotImplementedException();
-        }
-        /*        public async Task<Pagination<Quizz>> GetQuizzByUnitIdAsync(Guid UnitId, int pageNumber = 0, int pageSize = 10) => await _context.Quizzs.Where(p2 => p2.UnitId.Equals(UnitId)).ToListAsync();
-       public async Task<Pagination<Quizz>> GetQuizzByName(string Name, int pageNumber = 0, int pageSize = 10) => await _context.Quizzs.Where(x => x.QuizzName.Contains(Name)).ToListAsync();
-       public async Task<Pagination<Quizz>> GetDisableQuizzes(int pageNumber = 0, int pageSize = 10) => await _context.Quizzs.Where(x => x.Status == Domain.Enum.StatusEnum.Status.Disable).ToListAsync();
-       public async Task<Pagination<Quizz>> GetEnableQuizzes(int pageNumber = 0, int pageSize = 10) => await _context.Quizzs.Where(x => x.Status == Domain.Enum.StatusEnum.Status.Enable).ToListAsync();*/
+            var itemCount = await _context.Quizzs.CountAsync();
+            var items = await _dbSet.Where(x => x.UnitId.Equals(UnitId))
+                                    .OrderByDescending(x => x.CreationDate)
+                                    .Skip(pageNumber * pageSize)
+                                    .Take(pageSize)
+                                    .AsNoTracking()
+                                    .ToListAsync();
 
+            var result = new Pagination<Quizz>()
+            {
+                PageIndex = pageNumber,
+                PageSize = pageSize,
+                TotalItemsCount = itemCount,
+                Items = items,
+            };
+
+            return result;
+        }
     }
 }
