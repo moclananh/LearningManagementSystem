f3b4c87a27605529c30153d8045b2db69f9b1c99
PhanDucDat-SE150689 <ducdat010102@gmail.com>
2023-03-06 03:50:13 +0000

    Create GetEnable/DisableLecture, GetLectureByName/Id/UnitId

--- Applications.Test/Services/LectureServices/LectureServiceTests.cs
+++ Applications.Test/Services/LectureServices/LectureServiceTests.cs
@@ -117,6 +117,112 @@ namespace Applications.Tests.Services.LectureServices
             _unitOfWorkMock.Verify(x => x.LectureRepository.Update(It.IsAny<Lecture>()), Times.Never);
             _unitOfWorkMock.Verify(x => x.SaveChangeAsync(), Times.Never);
         }
+        [Fact]
+        public async Task GetEnableLectures_ShoulReturnCorrectData()
+        {
+            //arrange
+            var lectureMockData = new Pagination<Lecture>
+            {
+                Items = _fixture.Build<Lecture>()
+                                .Without(x => x.Unit)
+                                .With(x => x.Status, Domain.Enum.StatusEnum.Status.Enable)
+                                .CreateMany(30)
+                                .ToList(),
+                PageIndex = 0,
+                PageSize = 10,
+                TotalItemsCount = 30
+            };
+            var lectures = _mapperConfig.Map<Pagination<Lecture>>(lectureMockData);
+            _unitOfWorkMock.Setup(x => x.LectureRepository.GetEnableLectures(0, 10)).ReturnsAsync(lectureMockData);
+            var expected = _mapperConfig.Map<Pagination<LectureViewModel>>(lectures);
+            //act
+            var result = await _lectureService.GetEnableLectures();
+            //assert
+            _unitOfWorkMock.Verify(x => x.LectureRepository.GetEnableLectures(0, 10), Times.Once());
+        }
+        [Fact]
+        public async Task GetDisableLectures_ShoulReturnCorrectData()
+        {
+            //arrange
+            var lectureMockData = new Pagination<Lecture>
+            {
+                Items = _fixture.Build<Lecture>()
+                                .Without(x => x.Unit)
+                                .With(x => x.Status, Domain.Enum.StatusEnum.Status.Disable)
+                                .CreateMany(30)
+                                .ToList(),
+                PageIndex = 0,
+                PageSize = 10,
+                TotalItemsCount = 30
+            };
+            var lectures = _mapperConfig.Map<Pagination<Lecture>>(lectureMockData);
+            _unitOfWorkMock.Setup(x => x.LectureRepository.GetDisableLectures(0, 10)).ReturnsAsync(lectureMockData);
+            var expected = _mapperConfig.Map<Pagination<LectureViewModel>>(lectures);
+            //act
+            var result = await _lectureService.GetDisableLectures();
+            //assert
+            _unitOfWorkMock.Verify(x => x.LectureRepository.GetDisableLectures(0, 10), Times.Once());
+        }
+        [Fact]
+        public async Task GetLectureByName_ShouldReturnCorrectData()
+        {
+            //arrange
+            var MockData = new Pagination<Lecture>
+            {
+                Items = _fixture.Build<Lecture>()
+                                .Without(x => x.Unit)
+                                .With(x => x.LectureName, "Mock")
+                                .CreateMany(30)
+                                .ToList(),
+                PageIndex = 0,
+                PageSize = 10,
+                TotalItemsCount = 30,
+            };
+            var lectures = _mapperConfig.Map<Pagination<Lecture>>(MockData);
+            _unitOfWorkMock.Setup(x => x.LectureRepository.GetLectureByName("Mock", 0, 10)).ReturnsAsync(MockData);
+            var expected = _mapperConfig.Map<Pagination<LectureViewModel>>(lectures);
+            //act
+            var result = await _lectureService.GetLectureByName("Mock");
+            //assert
+            _unitOfWorkMock.Verify(x => x.LectureRepository.GetLectureByName("Mock", 0, 10), Times.Once());
+        }
+        [Fact]
+        public async Task GetLectureById_ShouldReturnCorrectData()
+        {
+            //arrange
+            var lectures = _fixture.Build<Lecture>()
+                                        .Without(x => x.Unit)
+                                        .Create();
+            _unitOfWorkMock.Setup(x => x.LectureRepository.GetByIdAsync(lectures.Id))
+                           .ReturnsAsync(lectures);
+            //act
+            var result = await _lectureService.GetLectureById(lectures.Id);
+            //assert
+            _unitOfWorkMock.Verify(x => x.LectureRepository.GetByIdAsync(lectures.Id), Times.Once());
+        }
+        [Fact]
+        public async Task GetLecutreByUnitId_ShouldReturnCorrectData()
+        {
+            //arrange
+            var id = Guid.NewGuid();
+            var lectureMockData = new Pagination<Lecture>
+            {
+                Items = _fixture.Build<Lecture>()
+                                .Without(x => x.Unit)
+                                .CreateMany(30)
+                                .ToList(),
+                PageIndex = 0,
+                PageSize = 10,
+                TotalItemsCount = 30,
+            };
+            var lecture = _mapperConfig.Map<Pagination<Lecture>>(lectureMockData);
+            _unitOfWorkMock.Setup(x => x.LectureRepository.GetLectureByUnitId(id, 0, 10)).ReturnsAsync(lectureMockData);
+            var expected = _mapperConfig.Map<Pagination<LectureViewModel>>(lecture);
+            //act
+            var result = await _lectureService.GetLectureByUnitId(id);
+            //assert
+            _unitOfWorkMock.Verify(x => x.LectureRepository.GetLectureByUnitId(id, 0, 10), Times.Once());
+        }
     }
 }
 
