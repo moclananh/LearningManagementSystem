dc6b8e498c976464faa2578744aa298adff74ca1
Văn Nghị Jr <vannghijr@gmail.com>
2023-03-06 08:01:38 +0000

    Group 03 - Test Practice

--- Infrastructures.Test/Repositories/PracticeRepositoryTest.cs
+++ Infrastructures.Test/Repositories/PracticeRepositoryTest.cs
@@ -1,4 +1,5 @@
-﻿using Applications.Repositories;
+﻿using Applications.IRepositories;
+using Applications.Repositories;
 using AutoFixture;
 using Domain.Entities;
 using Domain.Tests;
@@ -67,6 +68,64 @@ namespace Infrastructures.Tests.Repositories
             //act
             var resultPaging = await _practiceRepository.GetPracticeByUnitId(i);
             var result = resultPaging.Items.ToList();
+            //assert
+            resultPaging.Previous.Should().BeFalse();
+            resultPaging.Next.Should().BeTrue();
+            resultPaging.Items.Count.Should().Be(10);
+            resultPaging.TotalItemsCount.Should().Be(30);
+            resultPaging.TotalPagesCount.Should().Be(3);
+            resultPaging.PageIndex.Should().Be(0);
+            resultPaging.PageSize.Should().Be(10);
+            result.Should().BeEquivalentTo(expected);
+        }
+        [Fact]
+        public async Task PracticetRepository_GetEnablePractices_ShoulReturnCorrectData()
+        {
+            //arrange
+            var mockData = _fixture.Build<Practice>()
+                .Without(x => x.PracticeQuestions)
+                .Without(x => x.Unit)
+                .With(x => x.Status, Domain.Enum.StatusEnum.Status.Enable)
+                .CreateMany(30)
+                .ToList();
+            await _dbContext.Practices.AddRangeAsync(mockData);
+            await _dbContext.SaveChangesAsync();
+            var expected = mockData.Where(x => x.Status == Domain.Enum.StatusEnum.Status.Enable)
+                                    .OrderByDescending(x => x.CreationDate)
+                                    .Take(10)
+                                    .ToList();
+            //act
+            var resultPaging = await _practiceRepository.GetEnablePractices();
+            var result = resultPaging.Items;
+            //assert
+            resultPaging.Previous.Should().BeFalse();
+            resultPaging.Next.Should().BeTrue();
+            resultPaging.Items.Count.Should().Be(10);
+            resultPaging.TotalItemsCount.Should().Be(30);
+            resultPaging.TotalPagesCount.Should().Be(3);
+            resultPaging.PageIndex.Should().Be(0);
+            resultPaging.PageSize.Should().Be(10);
+            result.Should().BeEquivalentTo(expected);
+        }
+        [Fact]
+        public async Task PracticetRepository_GetDisablePractices_ShoulReturnCorrectData()
+        {
+            //arrange
+            var mockData = _fixture.Build<Practice>()
+                .Without(x => x.PracticeQuestions)
+                .Without(x => x.Unit)
+                .With(x => x.Status, Domain.Enum.StatusEnum.Status.Disable)
+                .CreateMany(30)
+                .ToList();
+            await _dbContext.Practices.AddRangeAsync(mockData);
+            await _dbContext.SaveChangesAsync();
+            var expected = mockData.Where(x => x.Status == Domain.Enum.StatusEnum.Status.Disable)
+                                    .OrderByDescending(x => x.CreationDate)
+                                    .Take(10)
+                                    .ToList();
+            //act
+            var resultPaging = await _practiceRepository.GetDisablePractices();
+            var result = resultPaging.Items;
             //assert
             resultPaging.Previous.Should().BeFalse();
             resultPaging.Next.Should().BeTrue();
