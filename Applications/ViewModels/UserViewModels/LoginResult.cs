4caf504b9ccfd7bef54193a536616b70ce4223cd
Hùng Nguyễn <viethungdev23@gmail.com>
2023-03-07 02:04:09 +0000

    Hung

--- Applications/ViewModels/UserViewModels/LoginResult.cs
+++ Applications/ViewModels/UserViewModels/LoginResult.cs
@@ -13,7 +13,7 @@ public class LoginResult
     public string Email { get; set; }
     public string Password { get; set; }
     public DateTime DOB { get; set; }
-    public String Gender { get; set; }
+    public string Gender { get; set; }
     public string Role { get; set; }
     public  string Status { get; set; }
     public string Token { get; set; }
