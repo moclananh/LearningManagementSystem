2a43f9cbf39bb8cceddc0c46aecb6c5008deafd9
Thanh Phuc <thanhphucmp@gmail.com>
2023-03-08 02:31:19 +0000

    Group3 - Test get practice by id

---
+++ Applications.Test/Services/AssignmentQuestionServices/AssignmentQuestionServicesTests.cs
@@ -0,0 +1,74 @@
+﻿using Applications.Interfaces;
+using Applications.Services;
+using ClosedXML.Excel;
+using Domain.Entities;
+using Domain.Tests;
+using FluentAssertions;
+using Moq;
+
+namespace Applications.Tests.Services.AssignmentQuestionServices
+{
+    public class AssignmentQuestionServicesTests : SetupTest
+    {
+        private readonly IAssignmentQuestionService _assignmentQuestionService;
+        public AssignmentQuestionServicesTests()
+        {
+            _assignmentQuestionService = new AssignmentQuestionService(_unitOfWorkMock.Object, _mapperConfig);
+        }
+
+        [Fact]
+        public async Task ExportAssignmentQuestionByAssignmentIdTest()
+        {
+            // Arrange
+            var assignmentId = Guid.NewGuid();
+            var expected = GetExpectedResult();
+
+            var mockService = new Mock<IAssignmentQuestionService>();
+            mockService.Setup(x => x.ExportAssignmentQuestionByAssignmentId(assignmentId)).ReturnsAsync(expected);
+
+            var service = mockService.Object;
+
+            // Act
+            var result = await service.ExportAssignmentQuestionByAssignmentId(assignmentId);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().BeEquivalentTo(expected, options => options
+                .WithStrictOrdering());
+        }
+
+        private byte[] GetExpectedResult()
+        {
+            var assId = Guid.NewGuid();
+            var questions = new List<AssignmentQuestion>
+            {
+                new AssignmentQuestion { Id = Guid.NewGuid(), AssignmentId = assId, Question = "Question 1", Answer = "Answer 1", Note = "Note 1" },
+                new AssignmentQuestion { Id = Guid.NewGuid(), AssignmentId = assId, Question = "Question 2", Answer = "Answer 2", Note = "Note 2" },
+                new AssignmentQuestion { Id = Guid.NewGuid(), AssignmentId = assId, Question = "Question 3", Answer = "Answer 3", Note = "Note 3" }
+            };
+
+            using var expectedWorkbook = new XLWorkbook();
+            var expectedWorksheet = expectedWorkbook.Worksheets.Add("Assignment Questions");
+            expectedWorksheet.Cell(1, 1).Value = "AsignmentID";
+            expectedWorksheet.Cell(2, 1).Value = "Question";
+            expectedWorksheet.Cell(2, 2).Value = "Answer";
+            expectedWorksheet.Cell(2, 3).Value = "Note";
+            expectedWorksheet.Cell(1, 2).Value = assId.ToString();
+            expectedWorksheet.Cell(3, 1).Value = "Question 1";
+            expectedWorksheet.Cell(3, 2).Value = "Answer 1";
+            expectedWorksheet.Cell(3, 3).Value = "Note 1";
+            expectedWorksheet.Cell(4, 1).Value = "Question 2";
+            expectedWorksheet.Cell(4, 2).Value = "Answer 2";
+            expectedWorksheet.Cell(4, 3).Value = "Note 2";
+            expectedWorksheet.Cell(5, 1).Value = "Question 3";
+            expectedWorksheet.Cell(5, 2).Value = "Answer 3";
+            expectedWorksheet.Cell(5, 3).Value = "Note 3";
+
+            using var expectedStream = new MemoryStream();
+            expectedWorkbook.SaveAs(expectedStream);
+            var expectedContent = expectedStream.ToArray();
+
+            return expectedContent;
+        }
+    }
+}
---
+++ Applications.Test/Services/PracticeServices/PracticeServicesTests.cs
@@ -0,0 +1,35 @@
+﻿using Applications.Interfaces;
+using Applications.Services;
+using Applications.ViewModels.PracticeViewModels;
+using AutoFixture;
+using Domain.Entities;
+using Domain.Tests;
+using FluentAssertions;
+using Moq;
+
+namespace Applications.Tests.Services.PracticeServices
+{
+    public class PracticeServicesTests : SetupTest
+    {
+        private readonly IPracticeService _practiceService;
+        public PracticeServicesTests()
+        {
+            _practiceService = new PracticeService(_unitOfWorkMock.Object, _mapperConfig);
+        }
+
+        [Fact]
+        public async Task GetPracticeById_ShouldReturnCorrectData()
+        {
+            var practiceMocks = _fixture.Build<Practice>()
+                                .Without(x => x.Unit)
+                                .Without(x => x.PracticeQuestions)
+                                .Create();
+            _unitOfWorkMock.Setup(x => x.PracticeRepository.GetByIdAsync(It.IsAny<Guid>())).ReturnsAsync(practiceMocks);
+            var expected = _mapperConfig.Map<PracticeViewModel>(practiceMocks);
+            //act
+            var result = _practiceService.GetPracticeById(practiceMocks.Id);
+            //assert
+            result.Result.Should().BeEquivalentTo(expected);
+        }
+    }
+}
