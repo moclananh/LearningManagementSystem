b920fe3e5fdc83fae91e84533468cb0c4ad82095
Văn Nghị Jr <vannghijr@gmail.com>
2023-03-30 08:16:46 +0000

    Group 3 - Test SyllabusModule

--- Applications.Test/Services/SyllabusModuleServices/SyllabusModuleServiceTests.cs
+++ Applications.Test/Services/SyllabusModuleServices/SyllabusModuleServiceTests.cs
@@ -1,4 +1,5 @@
-﻿using Applications.Interfaces;
+﻿using Applications.Commons;
+using Applications.Interfaces;
 using Applications.Services;
 using Applications.ViewModels.Response;
 using Applications.ViewModels.SyllabusModuleViewModel;
@@ -7,6 +8,7 @@ using Domain.Entities;
 using Domain.EntityRelationship;
 using Domain.Tests;
 using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 using System.Net;
 
@@ -66,6 +68,61 @@ namespace Applications.Tests.Services.SyllabusModuleServices
             var result = await _syllabusModuleService.AddMultiModulesToSyllabus(syllabusMockData.Id, ModulesListId);
             //assert
             result.Should().BeEquivalentTo(expected);
+        }
+        [Fact]
+        public async Task GetAllSyllabusModule_ShouldReturnCorrectData()
+        {
+            //arrange
+            var syllabusMockData = _fixture.Build<Syllabus>()
+                                        .Without(s => s.SyllabusModules)
+                                        .Without(s => s.SyllabusOutputStandards)
+                                        .Without(s => s.TrainingProgramSyllabi)
+                                        .CreateMany(30)
+                                        .ToList();
+            var moduleMockData = _fixture.Build<Module>()
+                                       .Without(x => x.AuditPlan)
+                                       .Without(x => x.ModuleUnits)
+                                       .Without(x => x.SyllabusModules)
+                                       .Create();
+            var user = _fixture.Build<User>()
+                              .Without(x => x.UserAuditPlans)
+                              .Without(x => x.AbsentRequests)
+                              .Without(x => x.ClassUsers)
+                              .Without(x => x.Attendences)
+                              .CreateMany(3)
+                              .ToList();
+            await _dbContext.Syllabi.AddRangeAsync(syllabusMockData);
+            await _dbContext.Modules.AddAsync(moduleMockData);
+            await _dbContext.SaveChangesAsync();
+            var MockData = new List<SyllabusModule>();
+            foreach (var item in syllabusMockData)
+            {
+                var mockData = new SyllabusModule
+                {
+                    Syllabus = item,
+                    Module = moduleMockData
+                };
+                MockData.Add(mockData);
+            }
+            var itemCount = await _dbContext.SyllabusModule.CountAsync();
+            var items = await _dbContext.SyllabusModule.OrderByDescending(x => x.CreationDate)
+                                                  .Take(10)
+                                                  .AsNoTracking()
+                                                  .ToListAsync();
+            var resultset = new Pagination<SyllabusModule>()
+            {
+                PageIndex = 0,
+                PageSize = 10,
+                TotalItemsCount = itemCount,
+                Items = items,
+            };
+            var expected = _mapperConfig.Map<Pagination<SyllabusModule>>(resultset);
+            _unitOfWorkMock.Setup(x => x.SyllabusModuleRepository.ToPagination(0, 10)).ReturnsAsync(resultset);
+            _unitOfWorkMock.Setup(x => x.UserRepository.GetEntitiesByIdsAsync(It.IsAny<List<Guid?>>())).ReturnsAsync(user);
+            //act
+            var result = await _syllabusModuleService.GetAllSyllabusModuleAsync();
+            //assert
+            _unitOfWorkMock.Verify(x => x.SyllabusModuleRepository.ToPagination(0, 10), Times.Once());
         }
     }
 }
