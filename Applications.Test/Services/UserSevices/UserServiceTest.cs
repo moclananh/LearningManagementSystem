390993bd24c5c587b3eddfebddd709a2abfa50ea
Hùng Nguyễn <viethungdev23@gmail.com>
2023-04-03 06:27:31 +0000

    Hung- test user

--- Applications.Test/Services/UserSevices/UserServiceTest.cs
+++ Applications.Test/Services/UserSevices/UserServiceTest.cs
@@ -1,13 +1,16 @@
 ﻿using Applications.Commons;
 using Applications.Interfaces;
 using Applications.Services;
+using Applications.Utils;
+using Applications.ViewModels.Response;
+using Applications.ViewModels.TokenViewModels;
 using Applications.ViewModels.UserViewModels;
 using AutoFixture;
 using Domain.Entities;
 using Domain.Tests;
 using FluentAssertions;
-using Microsoft.Extensions.Configuration;
 using Moq;
+using System.Net;
 
 namespace Applications.Tests.Services.UserSevices;
 
@@ -69,4 +72,178 @@ public class UserServiceTest : SetupTest
         result.Should().BeEquivalentTo(expected);
     }
 
+    [Fact]
+    public async Task GetAllUsers_shouldBeReturnData()
+    {
+        //arrage
+        var userMockData = new Pagination<User>
+        {
+            PageIndex = 0,
+            PageSize = 10,
+            TotalItemsCount = 30,
+            Items = _fixture.Build<User>()
+                .Without(x => x.AbsentRequests)
+                .Without(x => x.Attendences)
+                .Without(x => x.UserAuditPlans)
+                .Without(x => x.ClassUsers)
+                .CreateMany(30)
+                .ToList(),
+        };
+        _unitOfWorkMock.Setup(u => u.UserRepository.ToPagination(0, 10)).ReturnsAsync(userMockData);
+        var expected = _mapperConfig.Map<Pagination<UserViewModel>>(userMockData);
+        //act
+        var result = await _userService.GetAllUsers(0,10);
+        //assert
+        result.Should().BeEquivalentTo(expected);
+    }
+
+    [Fact]
+    public async Task GetUsersByRole_ShouldBeReturnCorrectData()
+    {
+        //arrage
+        var userMockData = new Pagination<User>
+        {
+            PageIndex = 0,
+            PageSize = 10,
+            TotalItemsCount = 30,
+            Items = _fixture.Build<User>()
+                .Without(x => x.AbsentRequests)
+                .Without(x => x.Attendences)
+                .Without(x => x.UserAuditPlans)
+                .Without(x => x.ClassUsers)
+                .With(x => x.Role, Domain.Enum.RoleEnum.Role.SuperAdmin)
+                .CreateMany(30)
+                .ToList(),
+        };
+        _unitOfWorkMock.Setup(u => u.UserRepository.GetUsersByRole(0,0,10)).ReturnsAsync(userMockData);
+        var expected = _mapperConfig.Map<Pagination<UserViewModel>>(userMockData);
+
+        //act
+        var result = await _userService.GetUsersByRole(0, 0, 10);
+
+        //assert
+        result.Should().BeEquivalentTo(expected);
+    }
+
+    [Fact]
+    public async Task AddUser_ShouldReturnCorrectData_WhenSuccessSaved()
+    {
+        //arrage
+        var userViewModelMock = _fixture.Build<CreateUserViewModel>()
+            .Create();
+        //_unitOfWorkMock.Setup(u => u.UserRepository.GetUserByEmail(userViewModelMock.Email)).Returns(null);
+        _unitOfWorkMock.Setup(u => u.UserRepository.AddAsync(It.IsAny<User>())).Returns(Task.CompletedTask);
+        _unitOfWorkMock.Setup(u => u.SaveChangeAsync()).ReturnsAsync(1);
+
+        //act
+        var result = await _userService.AddUser(userViewModelMock);
+
+        //assert
+        _unitOfWorkMock.Verify(s => s.UserRepository.AddAsync(It.IsAny<User>()), Times.Once());
+        _unitOfWorkMock.Verify(s => s.SaveChangeAsync(), Times.Once());
+    }
+
+    [Fact]
+    public async Task AddUser_ShouldReturnCorrectData_WhenSuccessFaild()
+    {
+        //arrage
+        var userViewModelMock = _fixture.Build<CreateUserViewModel>()
+            .Create();
+        //_unitOfWorkMock.Setup(u => u.UserRepository.GetUserByEmail(userViewModelMock.Email)).Returns(null);
+        _unitOfWorkMock.Setup(u => u.UserRepository.AddAsync(It.IsAny<User>())).Returns(Task.CompletedTask);
+        _unitOfWorkMock.Setup(u => u.SaveChangeAsync()).ReturnsAsync(0);
+
+        //act
+        var result = await _userService.AddUser(userViewModelMock);
+
+        //assert
+        _unitOfWorkMock.Verify(s => s.UserRepository.AddAsync(It.IsAny<User>()), Times.Once());
+        _unitOfWorkMock.Verify(s => s.SaveChangeAsync(), Times.Once());
+    }
+
+    [Fact]
+    public async Task AddUser_ShouldReturnBabRequest()
+    {
+        //arrage
+        var userViewModelMock = _fixture.Build<CreateUserViewModel>()
+            .Create();
+        var user = _fixture.Build<User>()
+                .Without(x => x.AbsentRequests)
+                .Without(x => x.Attendences)
+                .Without(x => x.UserAuditPlans)
+                .Without(x => x.ClassUsers)
+                .Create();
+        
+        _unitOfWorkMock.Setup(u => u.UserRepository.GetUserByEmail(userViewModelMock.Email));
+
+        //act
+        var result = await _userService.AddUser(userViewModelMock);
+
+        //assert
+        result.Should().NotBeNull();
+
+    }
+
+    [Fact]
+    public async Task FilterUser_ShouldBeReturnCorrectData()
+    {
+        //arrage
+        var filterMock = _fixture.Build<FilterUserRequest>()
+            .Create();
+        var userMockData = new Pagination<User>
+        {
+            PageIndex = 0,
+            PageSize = 10,
+            TotalItemsCount = 30,
+            Items = _fixture.Build<User>()
+                .Without(x => x.AbsentRequests)
+                .Without(x => x.Attendences)
+                .Without(x => x.UserAuditPlans)
+                .Without(x => x.ClassUsers)
+                .With(x => x.Role, Domain.Enum.RoleEnum.Role.SuperAdmin)
+                .CreateMany(30)
+                .ToList(),
+        };
+        _unitOfWorkMock.Setup(u => u.UserRepository.FilterUser(filterMock,0,10)).ReturnsAsync(userMockData);
+        var expected = _mapperConfig.Map<Pagination<UserViewModel>>(userMockData);
+        
+        //act
+        var result = await _userService.FilterUser(filterMock,0,10);
+
+        //assert
+        result.Should().BeEquivalentTo(expected);
+    }
+
+    [Fact]
+    public async Task Login_ShouldBeReturnCorrectData()
+    {
+        //arrage
+        var userMock = new UserLoginViewModel
+        {
+            Email = "mock",
+            Password = StringUtils.Hash("12345")
+        };
+        var expectedResponse = new Response(HttpStatusCode.OK, "authorized",It.IsAny<LoginResult>());
+
+        var user = _fixture.Build<User>()
+                .Without(x => x.AbsentRequests)
+                .Without(x => x.Attendences)
+                .Without(x => x.UserAuditPlans)
+                .Without(x => x.ClassUsers)
+                .With(x => x.Email, userMock.Email)
+                .With(x => x.Password, userMock.Password)
+                .Create();
+        var tokenMock = _fixture.Build<TokenModel>().Create();
+
+        _unitOfWorkMock.Setup(unitOfWork => unitOfWork.UserRepository.GetUserByEmail(userMock.Email)).ReturnsAsync(user);
+        _tokenServiceMock.Setup(service => service.GetToken(user.Email)).ReturnsAsync(tokenMock);
+
+
+        //act
+        var result = await _userService.Login(userMock);
+
+        //assert
+        result.Should().NotBeNull();
+        //_unitOfWorkMock.Verify(x => x.QuizzRepository.GetByIdAsync(mockdata.Id), Times.Once());
+    }
 }
